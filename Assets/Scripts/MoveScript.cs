583dd0bc00cc2457e759f8452b7f4ebd9f6502d0
Tim Sims <tim.sims86@gmail.com>
2014-03-10 17:55:51 +0800

    first init

---
+++ Assets/Scripts/MoveScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveScript : MonoBehaviour {
+
+
+	public Vector2 speed = new Vector2(5, 5);
+
+	public Vector2 direction = new Vector2(-1, 0);
+
+	protected Vector2 movement;
+
+
+	// Update is called once per frame
+	void Update () {
+		movement = new Vector2 (speed.x * direction.x,
+		                       speed.y * direction.y);
+	}
+
+	void FixedUpdate(){
+		rigidbody2D.velocity = movement;
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	// speed of the ship
+	public Vector2 speed = new Vector2(20, 20);
+
+	// store the movment
+	protected Vector2 movement;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {	
+
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+
+		// 4 - Movement per direction
+		movement = new Vector2(speed.x * inputX, 
+		                       speed.y * inputY);
+	}
+
+	void FixedUpdate(){
+		// 5 - Move the game object
+		rigidbody2D.velocity = movement;
+	}
+}
